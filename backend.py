1ef5b6cd8777e40a1a6306c5223c4a300ebd6d03
Daniel Korsun <daniel@Daniels-MacBook-Pro-2.local>
2018-09-15 15:45:28 -0400

    Initial function dev

--- backend.py
+++ backend.py
@@ -1,3 +1,29 @@
 import numpy
 
+test1 = "Multiply three and four"
+test2 = "Set x to three"
+test3 = "Output quote hello world end quote"
 
+
+"""
+1. Create list of words from input --> DONE
+2. Separate strings from (based on endquote)
+3. Create "phrases"
+    > [keyword, [arguments_list]]
+    > test1  --> ["multiply", [3,4]] 
+4. map phrases to output
+    > ["multiply", [3,4]] --> "3 * 4"
+5. write to new line in .py file
+"""
+
+def word_list(input_string):
+    #makes all the words in the input string lowercase
+    lowered_string = input_string.lower()
+    # splits up the lowercase sentence into individual words
+    return lowered_string.split(" ")
+
+test1_words = word_list(test1)
+print (test1_words)    
+
+
+    
